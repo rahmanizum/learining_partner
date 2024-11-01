1154dd630ea003b455336b5831fcf19f93abc819
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-01 16:28:20 +0530

    two way data binding

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
@@ -14,4 +15,5 @@ export class RolesComponent {
   isActive : boolean = true;
   currentDate : Date = new Date();
   inputType : string = "checkbox";
+  selectedState : string = "";
 }
