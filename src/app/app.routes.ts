f2ade05fa7b8202ae1797a992c513b5551bd68ae
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-03 16:53:39 +0530

    proper routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { MasterComponent } from './components/master/master.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ClientComponent } from './components/client/client.component';
 
 export const routes: Routes = [{
     path: '',
@@ -6,5 +9,13 @@ export const routes: Routes = [{
     pathMatch: 'full'
 }, {
     path: 'master',
-    loadComponent : () => import('./components/master/master.component').then(m => m.MasterComponent)
-}];
+    component: MasterComponent
+},{
+    path: 'employee',
+    component: EmployeeComponent
+},{
+    path: 'client',
+    component: ClientComponent
+}
+
+];
