260a7d0d4e4371c4502524814924e77039b823c2
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-03 17:07:56 +0530

    using @if for loading

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -11,13 +11,17 @@ import { APIResponseModel, IDesignation } from '../../model/class/interface/role
 })
 export class DesignationComponent implements OnInit {
   designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
+  isLoading = true;
   ngOnInit(): void {
     this.masterService.getDesignationList().subscribe((res:APIResponseModel) => {
       this.designationList = res.data;
+      this.isLoading = false
     }, (err) => {
       alert(err)
+      this.isLoading = false
     });
   }
 
-  masterService = inject(MasterService);
+  
 }
