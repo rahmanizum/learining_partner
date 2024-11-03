7ef8412925bc4aa2b6d80d1072fc47ed8ae6b719
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-03 16:39:59 +0530

    one way of routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    redirectTo: 'master',
+    pathMatch: 'full'
+}, {
+    path: 'master',
+    loadComponent : () => import('./components/master/master.component').then(m => m.MasterComponent)
+}];
