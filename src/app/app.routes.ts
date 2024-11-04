040241eb277b0903893f1c94c818e5ae3cbe8ee3
Mufil Rahman A <mufil.r@simplicontract.com>
2024-11-04 12:20:50 +0530

    improvising routing and adding layout and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,32 @@ import { MasterComponent } from './components/master/master.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { ClientComponent } from './components/client/client.component';
 import { ClientProjectComponent } from './components/client-project/client-project.component';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [{
     path: '',
-    redirectTo: 'master',
+    redirectTo: 'login',
     pathMatch: 'full'
-}, {
-    path: 'master',
-    component: MasterComponent
 },{
-    path: 'employee',
-    component: EmployeeComponent
+    path: 'login',
+    component: LoginComponent
 },{
-    path: 'client',
-    component: ClientComponent
-},{
-    path:'client-project',
-    component: ClientProjectComponent
+    path: '',
+    component: LayoutComponent,
+    children: [{
+        path: 'master',
+        component: MasterComponent
+    },{
+        path: 'employee',
+        component: EmployeeComponent
+    },{
+        path: 'client',
+        component: ClientComponent
+    },{
+        path:'client-project',
+        component: ClientProjectComponent
+    }]
 }
 
 ];
